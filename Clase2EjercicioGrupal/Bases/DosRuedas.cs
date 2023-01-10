2527f31d04dc63a8a6c99298f8bdc214bde665f2
Anais Moreno <anabet.94@gmail.com>
2023-01-10 16:57:43 -0300

    hola

---
+++ Clase2EjercicioGrupal/Bases/DosRuedas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2EjercicioGrupal.Bases
+{
+    public class DosRuedas:MedioTransporte
+    {
+        public DosRuedas() { }
+
+    }
+}
---
+++ Clase2EjercicioGrupal/Bases/MedioTransporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2EjercicioGrupal.Bases
+{
+    public abstract class MedioTransporte
+    {
+
+        public MedioTransporte() { }
+        public MedioTransporte(string Nombre, string Modelo)
+        {
+            this.Nombre = Nombre;
+            this.Modelo = Modelo;
+        }
+        public string Nombre { get; set; }
+        public string Modelo { get; set; }
+    }
+}
