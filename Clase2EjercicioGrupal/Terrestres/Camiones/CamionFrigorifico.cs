0b90ec3ede42bb261d6c39258512987bc81716a0
Anais Moreno <anabet.94@gmail.com>
2023-01-11 14:52:41 -0300

    agregar clase CamionFrigorifico

---
+++ Clase2EjercicioGrupal/Terrestres/Camiones/CamionFrigorifico.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase2EjercicioGrupal.Bases;
+
+namespace Clase2EjercicioGrupal.Terrestres.Camiones
+{
+    internal class CamionFrigorifico:Camion
+    {
+        public int Frigorias { get; set; }
+        public string Marca { get; set; }
+        public CamionFrigorifico(string Nombre, string Modelo, string TipoMotor, int Capacidad, int Frigorias, string Marca) : base(Nombre, Modelo, TipoMotor, Capacidad)
+        {
+            this.Frigorias = Frigorias;
+            this.Marca = Marca;
+        }
+    }
+}
